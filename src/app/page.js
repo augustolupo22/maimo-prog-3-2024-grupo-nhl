af970b75d2ddea5786f007d4529635ff4ae731d7
ironiizx <66000342+ironiizx@users.noreply.github.com>
2024-05-30 17:33:14 -0300

    api actualizada

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
-import Navbar from "./Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import CardsGrid from "./components/CardsGrid";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
+      <CardsGrid/>
     </main>
   );
 }
