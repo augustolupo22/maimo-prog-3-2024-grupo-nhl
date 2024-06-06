49cc5dabe724b0ebd5bbe33ac4f5f263827a0f41
ironiizx <66000342+ironiizx@users.noreply.github.com>
2024-06-06 06:32:45 -0300

    Añadido de imagenes, boton, rediseño y grilla

--- src/app/components/CardsGrid.js
+++ src/app/components/CardsGrid.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
+import styles from './CardsGrid.module.css';
 
 const CardsGrid = () => {
     const [data, setData] = useState([]);
@@ -12,7 +13,7 @@ const CardsGrid = () => {
         const getData = async () => {
             try {
                 const response = await axios.get(
-                    'https://www.themealdb.com/api/json/v1/1/search.php?s=Sushi'
+                    'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
                 );
                 setData(response.data.meals);
                 setLoading(false);
@@ -26,17 +27,24 @@ const CardsGrid = () => {
 
     return (
         <div>
+            <h1 className={styles.welcome}>Bienvenido a Ver las Comidas</h1>
             {loading && <p>Loading ...</p>}
-            {!loading && data.length > 0 && (
-                <> 
-                    <Image
-                        src={data[0].strMealThumb}
-                        width={200}
-                        height={200}
-                        alt={data[0].strMeal}
-                    />
-                    <h2>{data[0].strMeal}</h2>
-                </>
+            {!loading && (
+                <div className={styles.grid}>
+                    {data.map((item, index) => (
+                        <div key={item.idMeal} className={`${styles.card} ${index < 3 ? styles.featured : ''}`}>
+                            <Image
+                                src={item.strMealThumb}
+                                width={400} // Ajusta el tamaño de la imagen aquí para los elementos destacados
+                                height={400} // Ajusta el tamaño de la imagen aquí para los elementos destacados
+                                alt={item.strMeal}
+                                className={styles.image}
+                            />
+                            <h2>{item.strMeal}</h2>
+                            <button className={styles.button}>Ver más</button>
+                        </div>
+                    ))}
+                </div>
             )}
         </div>
     );
