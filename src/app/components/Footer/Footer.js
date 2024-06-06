65b513322c354e2c8c76b8e3492acb01c85a7749
augusto lupo <augustolupo030422@gmail.com>
2024-06-06 17:44:20 -0300

    Actualizacion del Footer

---
+++ src/app/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.column}>
+          <h3>About Us</h3>
+          <p>Derechos de autor - @Copyright</p>
+        </div>
+        <div className={styles.column}>
+          <h3>Links</h3>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre nosotros</a></li>
+            <li><a href="">Contacto</a></li>
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <h3>Contactos</h3>
+          <ul className={styles.socials}>
+            <li><a href="https://facebook.com">Facebook</a></li>
+            <li><a href="https://twitter.com">Twitter</a></li>
+            <li><a href="https://instagram.com">Instagram</a></li>
+          </ul>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 import styles from "./page.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import CardsGrid from "./components/CardsGrid/CardsGrid";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <CardsGrid
-       className={styles.main}
+      <CardsGrid className={styles.main}
       />
+      <Footer />
     </main>
   );
 }
